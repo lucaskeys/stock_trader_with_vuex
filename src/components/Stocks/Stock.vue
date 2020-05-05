4ea5a1b9f43540e861cd4368396ac97e9b8aad67
lk <lucaskeys@gmail.com>
2020-05-05 12:02:25 -0700

    Set up stock buy function

--- src/components/Stocks/Stock.vue
+++ src/components/Stocks/Stock.vue
@@ -2,17 +2,38 @@
   <div class="col-sm-6 col-md-4">
     <div class="panel panel-success">
       <div class="panel-heading">
-        <h3 class="panel-title">Stock Name</h3>
-        <small>(Price: PRICE)</small>
+        <h3 class="panel-title">{{stock.name}}</h3>
+        <small>{{stock.price}}</small>
       </div>
       <div class="panel-body">
         <div class="pull-left">
-          <input type="number" class="form-control" placeholder="Quantity">
+          <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
         </div>
         <div class="pull-right">
-          <button class="btn btn-success">Buy</button>
+          <button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || Number.isInteger(quantity)">Buy</button>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+  export default {
+    props: ['stock'],
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: parseInt(this.quantity)
+        };
+        console.log(order)
+        this.quantity = 0;
+      }
+    }
+  }
+</script>
