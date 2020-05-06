5b282798e14118800c187209abd697843df10156
lk <lucaskeys@gmail.com>
2020-05-06 11:58:54 -0700

    add save funtion

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,10 +15,10 @@
         <strong class="navbar-text navbar-right">Funds: {{funds | currency }}</strong>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="#" @click="endDay">End Day</a></li>
-          <li class="dropdown">
+          <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
             <ul class="dropdown-menu">
-              <li><a href="#">Save Data</a></li>
+              <li><a href="#" @click="saveData">Save Data</a></li>
               <li><a href="#">Load Data</a></li>
             </ul>
           </li>
@@ -30,6 +30,11 @@
 <script>
   import {mapActions} from 'vuex'
   export default {
+    data() {
+      return {
+        isDropdownOpen: false
+      }
+    },
     computed: {
       funds() {
         return this.$store.getters.funds
@@ -41,6 +46,14 @@
       ]),
       endDay() {
         this.randomizeStocks();
+      },
+      saveData() {
+        const data = {
+          funds: this.$store.getters.funds,
+          stockPortfolio: this.$store.getters.stockPortfolio,
+          stocks: this.$store.getters.stocks
+        };
+        this.$http.put('data.json', data);
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import {routes} from './routes';
 import store from './store/store'
+import VueResource from 'vue-resource'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-5bc44.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
-})
+});
+
+
 
 const router = new VueRouter({
   mode: 'history',
